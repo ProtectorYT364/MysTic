70ce65f51fa871896204916f8265e551ce18aaf0
ProtectorYT364 <86392268+ProtectorYT364@users.noreply.github.com>
2021-11-04 11:23:52 +0530

    not tested

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -39,15 +39,15 @@ interface ProtocolInfo{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	public const CURRENT_PROTOCOL = 354;
+	public const CURRENT_PROTOCOL = 371;
 	/**
 	 * Current Minecraft PE version reported by the server. This is usually the earliest currently supported version.
 	 */
-	public const MINECRAFT_VERSION = 'v1.11.4';
+	public const MINECRAFT_VERSION = 'v1.17.40';
 	/**
 	 * Version number sent to clients in ping responses.
 	 */
-	public const MINECRAFT_VERSION_NETWORK = '1.11.4';
+	public const MINECRAFT_VERSION_NETWORK = '1.17.40';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
